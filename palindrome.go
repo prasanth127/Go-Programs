8589d14150f015aae1f89e4abdd30124fdaaae32
kudumala prasanth <95011426+prasanth127@users.noreply.github.com>
2022-05-19 09:48:55 +0530

    Add files via upload

---
+++ palindrome.go
@@ -0,0 +1,31 @@
+//go: palindrome program
+
+//package word
+package main
+
+import (
+	"unicode"
+)
+
+func IsPalindrome1(s string) bool {
+	for i := range s { //a2a//len =3, start_index = 0; last_inde = 2
+		if s[i] != s[len(s)-1-i] { //i=0;s[0] == s[2] //s[1] =2; s[1] = 2
+			return false
+		}
+	}
+	return true
+}
+func IsPalindrome(s string) bool {
+	var letters []rune //rune is a UTF8 unicode character in golang
+	for _, r := range s {
+		if unicode.IsLetter(r) {
+			letters = append(letters, unicode.ToLower(r))
+		}
+	}
+	for i := range letters {
+		if letters[i] != letters[len(letters)-1-i] {
+			return false
+		}
+	}
+	return true
+}
